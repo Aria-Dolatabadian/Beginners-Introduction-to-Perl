01fd47972270b23c52c8785d6ad1527311bad980
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2024-10-03 13:49:13 +0800

    Create Code.pl

---
+++ Code.pl
@@ -0,0 +1,30 @@
+#!/usr/local/bin/perl
+#Variable
+$a =5;
+$b= "Aria";
+$a2 = "Hi";
+print "$a\n";
+print "\n";
+print "$b\n";
+print "\n";
+print "$a2\n";
+print "\n\n\n\n\n\n\n";
+
+print "The report is: $b\n";
+
+print "\n";
+
+$c = $a + 3;
+print "$c\n";
+
+$d = $a + $b;
+print "$d\n";
+
+$e =20;
+$e++;   # $e is now 21; we added 1 to it.
+print "$e\n";
+$e+=10; #Now it's 31; we added 10.
+print "$e\n";
+$e /=2; # And divided it by 2, so it's 15.5
+print "$e\n";
+
